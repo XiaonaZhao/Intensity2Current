a17ae2a8e98e6d0eae42391738cc26c32c15a196
Nona Zhao <nonazhao@mail.ustc.edu.cn>
2022-12-13 22:13:08 +0800

    Add files via upload

---
+++ ReadTifROI.m
@@ -0,0 +1,15 @@
+function tif = ReadTifROI(fileFile, mask)
+% Something wrong and I don't know how to modify this file.
+
+[tifFolder, tifNames] = ReadTifFileNames(fileFile);
+row = size(tifNames, 1);
+
+tif = cell(row, 5);
+tif0 = double(imread(fullfile(tifFolder, tifNames{1})));
+for n = 1: row
+    tif{n, 2} = n; % sequence
+    tif{n, 3} = (double(imread(fullfile(tifFolder, tifNames{n}))) - tif0).*mask;
+%     tif{n, 4} = ROImean(tif{n, 3}, mask); % MoS2 need this line.
+end
+
+end
