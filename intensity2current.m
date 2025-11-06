ed5afd9c9f38e04145296c2ee4c44425a1a3a39a
Nona Zhao <nonazhao@mail.ustc.edu.cn>
2025-11-06 22:35:25 +0800

    Update intensity2current.m

--- intensity2current.m
+++ intensity2current.m
@@ -1,15 +1,14 @@
-function Current = intensity2current(Intensity, imgNum)
-% The reaction on the electrode surface: H2O + e- = H + OH-
+function Current = intensity2current(Intensity, imgNum, fps)
+% 本项目针对Nikon ti-E，60倍物镜，两倍视野转接头下滨松相机16
 
-n = -1;    % Number of electrons transferred in the cell;
+n = 1;    % Number of electrons transferred in the cell;
 F = 9.65e4;   % Faraday constant;
 D = 1e-9; % Actually, Do & Dr are ~ (5 - 8)e-9
 
-AlphaOxi = 7.34;
+AlphaOxi = 7.34;   % ===========================================
 AlphaRe = 7.34./2;
 Constant = (-1)*(AlphaOxi-AlphaRe)/(D^(0.5))/(n*F*(pi^(0.5)));
-% frameRate = 106; % PIKE
-frameRate = 100; % HAMAMATSU
+frameRate = fps; % HAMAMATSU
 Time = (1:imgNum)./frameRate;
 DeltaT = ones(imgNum,1)./frameRate;
 
@@ -33,6 +32,6 @@ for i = 3 : imgNum
     Sum_new(i) = sum(real(Current(1:i-2)'./Sub(i-2,1:i-2))).*(DeltaT(i));
     Current(i-1) = (Intensity(i-1)./Constant - Sum_new(i))./abs(DeltaT(i-1)).*((Time(i)-Time(i-1)).^(0.5));
 end
-Current = Current*(-1);
+
 end
 
